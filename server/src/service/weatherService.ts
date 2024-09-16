06cbfcf64d36db13609d497308e29d4244d11d69
Deepthi Sandeep <deepththampi@hotmail.com>
2024-09-16 16:07:08 -0500

    updated WeatherService class,WeatherRoutes class  and functions-003

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -12,7 +12,7 @@ class Weather {
   
   constructor(
     public city:string,
-    public date:Date,
+    public date:string,
     public icon:string,
     public weatherDescription: string,
     public temperature: number,
@@ -32,6 +32,8 @@ class Weather {
     
   }
 }
+
+
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
@@ -40,6 +42,7 @@ class WeatherService {
    private baseURL = process.env.API_BASE_URL || "https://api.openweathermap.org";
    private API_KEY = process.env.API_KEY;
    private city: string = "";
+
   // TODO: Create fetchLocationData method
    private async fetchLocationData(query: string) {
     const response = await fetch(query);
@@ -47,12 +50,16 @@ class WeatherService {
     const data = await response.json();
     return data;
    }
+
+
   // TODO: Create destructureLocationData method
    private destructureLocationData(locationData: Coordinates): Coordinates {
     const { lat, lon } = locationData;
     
     return { lat, lon };
    }
+
+
   // TODO: Create buildGeocodeQuery method
    private buildGeocodeQuery(city : string): string {
    this.city = city;
@@ -60,11 +67,15 @@ class WeatherService {
     //console.log("Value retured from API : ",query);
     return `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.API_KEY}`;
    }
+
+
   // TODO: Create buildWeatherQuery method
    private buildWeatherQuery(coordinates: Coordinates): string {
     const { lat, lon } = coordinates;
     return `${this.baseURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${this.API_KEY}`;
    }
+
+
   // TODO: Create fetchAndDestructureLocationData method
    private async fetchAndDestructureLocationData(city: string) {
     this.city = city;
@@ -75,12 +86,14 @@ class WeatherService {
     // console.log("LLLLOOOCCCAAATT ",locationData);
     return this.destructureLocationData(locationData.coord);
    }
+
   // TODO: Create fetchWeatherData method
    private async fetchWeatherData(coordinates: Coordinates) {
     const weatherQuery = this.buildWeatherQuery(coordinates);
     //console.log(weatherQuery);
     return await this.fetchLocationData(weatherQuery);
    }
+
   // TODO: Build parseCurrentWeather method
    private parseCurrentWeather(response: any) {
     //console.log("Weather API Response (inside parseCurrentWeather):", response);
@@ -89,7 +102,7 @@ class WeatherService {
     }
     // Extract necessary data from the response
   //const city = response.name;//cityName;
-  const date = new Date(response.dt * 1000); 
+  const date = new Date(response.dt * 1000).toLocaleDateString()||'0'; 
   const icon = response.weather[0].icon;
   const temperature = response.main.temp;
   const humidity = response.main.humidity;
@@ -110,12 +123,14 @@ class WeatherService {
     //console.log("Curretn weather - ParseCurrentWeather",currentWeather);
     return currentWeather;
    }
+
+
   // TODO: Complete buildForecastArray method
    private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
     const forecastArray = weatherData.map((entry) => {
       return new Weather(
         this.city,
-        new Date(entry.dt * 1000),
+        new Date(entry.dt * 1000).toLocaleDateString()||'0',
         entry.weather[0].icon,
         entry.weather[0].description,
         entry.main.temp,
@@ -129,6 +144,8 @@ class WeatherService {
     //console.log("Current Weather - buildForecastArray ",currentWeather);
     return [currentWeather, ...forecastArray];
    }
+   
+
   // TODO: Complete getWeatherForCity method
    async getWeatherForCity(city: string) {
    
@@ -148,7 +165,7 @@ class WeatherService {
     const forecastArray = this.buildForecastArray(currentWeather, weatherData.list);
     //const forecasts = weatherData.list.slice(1).map((item: any) => parseForecast(item));
     //console.log("FFFFFFFFFFFFFFFFF getWeatherForCity - forecastArray() ",forecastArray);
-    return [currentWeather,forecastArray];
+    return [forecastArray];
 
 }
 }
