a39d71a74848a2abb0428db2608a4dc6175eb7cb
Deepthi Sandeep <deepththampi@hotmail.com>
2024-09-18 18:13:03 -0500

    updated path in server.ts-02

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
- //app.use(express.static('../../client/dist'));
- app.use(express.static("../client/dist"));
+ app.use(express.static('../../client/dist'));
+ //app.use(express.static("../client/dist"));
 //app.use(express.static('../../../client/dist'));
 // app.use(express.static(path.join(__dirname, '../client/dist')));
 
